a9b81d0362484e318a266ad7a27fbba363c24ce8
Ivan Lit <litvin.ivan.ak@gmail.com>
2020-06-08 17:26:43 +0300

    39. ActionCreator

--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST'
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+
 let store = {
     _state: {
         profilePage: {
@@ -33,7 +36,7 @@ let store = {
         this._callSubscribe = observer;
     },
     dispatch(action) {
-      if (action.type === 'ADD-POST') {
+      if (action.type === ADD_POST) {
           let newPost = {
               id: 3,
               post: this._state.profilePage.newPostText
@@ -41,11 +44,14 @@ let store = {
           this._state.profilePage.posts.push(newPost);
           this._state.profilePage.newPostText = '';
           this._callSubscribe(this._state);
-      }  else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+      }  else if (action.type === UPDATE_NEW_POST_TEXT) {
           this._state.profilePage.newPostText = action.newText;
           this._callSubscribe(this._state)
       }
     }
 }
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text})
+
 export default store;
 window.store = store;
