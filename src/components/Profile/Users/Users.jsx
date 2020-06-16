9733dadc59b70cd408fe48094c6c3d5d970ceeeb
root <litvin.ivan.ak@gmail.com>
2020-06-16 12:45:31 +0300

    Class Users

--- src/components/Profile/Users/Users.jsx
+++ src/components/Profile/Users/Users.jsx
@@ -1,47 +1,52 @@
 import React from "react";
 import style from "./Users.module.css";
-import * as axios from "axios"
-import userPhoto from "../../../assets/images/user.png"
+import userPhoto from "../../../assets/images/user.png";
+import * as axios from "axios";
 
 
-let Users = (props) => {
-    if (props.users.length === 0) {
+
+class Users extends React.Component {
+    constructor(proos) {
+        super(proos);
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             }
         )
     }
-    return (
-        <div className={style.NavbarWrapper}>
-            <div className={style.Container}>
-                <section>
-                    {
-                        props.users.map(user => <div key={user.id}>
-                            <div>
-                                <img src={user.photoUrl != null ? user.photos.small : userPhoto} className={style.userPhoto}/>
-                            </div>
-                            <div>
-                                {user.followed ?
-                                    <button onClick={() => {
-                                        props.unfollow(user.id)
-                                    }}>Follow</button>
-                                    : <button onClick={() => {
-                                        props.follow(user.id)
-                                    }}>Unfollow</button>}
-                            </div>
-                            <span>
+    render() {
+        return (
+            <div className={style.NavbarWrapper}>
+                <div className={style.Container}>
+                    <section>
+                        {
+                            this.props.users.map(user => <div key={user.id}>
+                                <div>
+                                    <img src={user.photos.small != null ? user.photos.small : userPhoto} className={style.userPhoto}/>
+                                    {/*если с API пришло photos.small, и оно не null, отобрази его, иначе отрисуй шаблонную фотку юзера, которую мы захардкодили импортом*/}
+                                </div>
+                                <div>
+                                    {user.followed ?
+                                        <button onClick={() => {
+                                            this.props.unfollow(user.id)
+                                        }}>Follow</button>
+                                        : <button onClick={() => {
+                                            this.props.follow(user.id)
+                                        }}>Unfollow</button>}
+                                </div>
+                                <span>
                                 <div>{user.fullName}</div>
                                 <div>{user.status}</div>
                             </span>
-                            <span>
+                                <span>
                                 <div>'user.location.country'</div>
                                 <div>'user.location.city'</div>
                             </span>
-                        </div>)
-                    }
-                </section>
+                            </div>)
+                        }
+                    </section>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 export default Users;
