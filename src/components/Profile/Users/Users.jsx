61aa878709087ae27de9c6f08742f015d770c05e
Ivan Lit <litvin.ivan.ak@gmail.com>
2020-06-17 20:18:47 +0300

    Users Pagination

--- src/components/Profile/Users/Users.jsx
+++ src/components/Profile/Users/Users.jsx
@@ -6,18 +6,37 @@ import * as axios from "axios";
 
 
 class Users extends React.Component {
-    constructor(proos) {
-        super(proos);
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
                 this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
             }
         )
     }
+    onPageChanged (pageNumber) {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items)
+            }
+        )
+
+    }
     render() {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
         return (
             <div className={style.NavbarWrapper}>
                 <div className={style.Container}>
                     <section>
+                        <div className={style.buttonPage}>
+                            {pages.map( p => {
+                                return <a className={this.props.currentPage === p && style.selectedPage}
+                                onClick={(e) => {this.onPageChanged(p)}}>{p}</a>
+                            })}
+                        </div>
                         {
                             this.props.users.map(user => <div key={user.id}>
                                 <div>
